dc03a3c4c3f303023a558064a6b8c300f18ae19d
Gonçalo Estrelado <goncalo.estrelado@hotmail.com>
2022-01-31 05:07:07 +0000

    done iteration 4

--- app.js
+++ app.js
@@ -1,18 +1,67 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require('express')
+const hbs = require('hbs')
+const mongoose = require('mongoose')
+const async = require('hbs/lib/async')
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node')
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+// connexion to mongodb
+mongoose.connect('mongodb://localhost/Ironhack-spotify-api')
+
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
+app.use(express.static(__dirname + '/public'))
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+})
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch((error) =>
+    console.log('Something went wrong when retrieving an access token', error)
+  )
 
 // Our routes go here:
+app.get('/', (req, res, next) => {
+  res.render('home')
+})
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get('/artist-search', (req, res, next) => {
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then((data) => {
+      const artists = data.body.artists.items
+      console.log(artists.forEach((element) => console.log(element)))
+      res.render('artist-search-results', { artists })
+    })
+    .catch((err) =>
+      console.log('The error while searching artists occurred: ', err)
+    )
+})
+
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then((data) => {
+      console.log('The received data from the API: ', data.body.items);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.render('albums', { albums: data.body.items })
+    })
+    .catch((err) =>
+      console.log('The error while searching artists occurred: ', err)
+    )
+})
+
+app.listen(3000, () =>
+  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
+)
